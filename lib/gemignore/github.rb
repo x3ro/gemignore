7cc7a1921c5a99b3cbe256f84c48ae08b2bada50
Lucas Jenss <lucas@x3ro.de>
2012-05-10 01:53:35 +0200

    Write cache object to temporary file to reduce API requests

--- lib/gemignore/github.rb
+++ lib/gemignore/github.rb
@@ -1,12 +1,37 @@
 require 'octokit'
 require 'base64'
+require 'tmpdir'
+require 'json'
 
 # This class implements the necessary high-level operations we need to perform on the
 # GitHub API and caches them. "High-level", because most of them are already pre-processed
 # to fit what is needed in gemignore.
 #
 class GitHub
-  @cache = { }
+
+  # Path to our temporary cache file
+  @tmpFile = Dir.tmpdir + '/' + 'gemignoretemp'
+
+
+  # Try to load cache from temporary file
+  if File.exists? @tmpFile
+    @cache = JSON.parse(File.open(@tmpFile,'rb').read)
+  else
+    @cache = {}
+  end
+
+
+  # These readers are needed so that we can access the variables in the finalizer
+  class << self
+    attr_reader :tmpFile, :cache
+  end
+
+
+  # We write the cache object to a temporary file when the program exits
+  ObjectSpace.define_finalizer(self, proc do
+    File.open(self.tmpFile, 'w').write(JSON.generate(self.cache))
+  end)
+
 
   # Retrieve the full list of files available inside the given repository + tree.
   #
