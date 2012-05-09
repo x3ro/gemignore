f492959fbe3c344d2349fc565a4ec1984128fc4d
Lucas Jenss <lucas@x3ro.de>
2012-05-10 00:29:08 +0200

    Also add load path when requiring through IRB (for development)

--- lib/gemignore.rb
+++ lib/gemignore.rb
@@ -1,5 +1,8 @@
 module GemIgnore
 end
 
+lib = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift(lib) if File.directory?(lib) && !$LOAD_PATH.include?(lib)
+
 require 'gemignore/github'
 require 'gemignore/main'
