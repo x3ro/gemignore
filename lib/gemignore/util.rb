47256d84dee7f7d41552562bdeffb644a2b1e233
Lucas Jenss <lucas@x3ro.de>
2012-05-14 00:40:24 +0200

    More visual code cleanup

--- lib/gemignore/util.rb
+++ lib/gemignore/util.rb
@@ -5,24 +5,25 @@ module GemIgnore
       puts (prefix(level) + msg)
     end
 
+
     def success(msg, level=0)
       puts (prefix(level) + msg).foreground(:green)
     end
+
 
     def notice(msg, level=0)
       puts (prefix(level) + msg).foreground(:yellow)
     end
 
+
     def error(msg, level=0)
       puts (prefix(level) + msg).foreground(:red)
     end
+
 
     def prefix(level)
       return "" if level <= 0
       ("-" * level) + "> "
     end
-
-
-
   end
 end
